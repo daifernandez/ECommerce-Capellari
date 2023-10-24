ee09502df2c6ae2755ca270ceb74d692d33967db
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-10-23 23:32:47 -0300

    se modifica estructura del proyecto

---
+++ components/title.js
@@ -0,0 +1,68 @@
+export default function Title() {
+ return (
+    <div className="bg-gray-900">
+    <div className="isolate overflow-hidden pt-14 relative">
+      {/* <img
+        src="https://integracionempresaria.com.ar/wp-content/uploads/2021/03/cuidado-electrodomesticos.jpg"
+        alt="Electrodomésticos de Capellini"
+        className="absolute inset-0 z-[10] h-full w-full object-cover opacity-20"
+      /> */}
+      <div
+        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
+      </div>
+      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="text-center">
+          <h1 className="text-6xl font-serif tracking-tight text-white">
+            Capellari
+          </h1>
+          <p className="mt-6  leading-8 text-gray-300">
+            Electrodomésticos de alta calidad para tu hogar.
+          </p>
+          <div className="sm:mb-8 sm:flex sm:justify-center mb-6 mt-8">
+            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-yellow-500">
+              Descubre la calidad y tradición de Capellari.{" "}
+              <a href="#" className="font-semibold text-white">
+                <span className="absolute inset-0" aria-hidden="true" />
+                Ver más <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+          <p className="mt-10 text-lg leading-8 text-gray-300">
+            En Capellari, nos enorgullece nuestra larga tradición en la venta
+            de productos confiables y eficientes para tu vida diaria.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-md bg-yellow-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-yellow-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+            >
+              Ir a la tienda
+            </a>
+          </div>
+        </div>
+      </div>
+      <div
+        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
+      </div>
+    </div>
+  </div>
+ )   
+}
