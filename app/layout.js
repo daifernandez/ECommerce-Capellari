256b0e6f2445b5707ad563c4d9eb57c2b8d6638e
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-10-27 23:20:04 -0300

    inicio de maquetado, agrego componentes y modifico metadata

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,12 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "tailwindcss/tailwind.css";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Coder Next App',
-  description: 'Ecommerce de electrodomésticos, Capellari',
-}
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,21 @@
-import Title from "@/components/title";
+import Title from "../components/ui/title";
+import Header from "../components/ui/header";
+import Footer from "../components/ui/footer";
+
+export const metadata = {
+  title: "Coderhouse Next App || Daiana Fernandez",
+  description:
+    "Pagina de aplicacion Next.Js creada por Daiana Fernandez para Coderhouse S.R.L - Ecommerce de electrodomésticos, Capellari",
+};
 
 export default function Home() {
   return (
-    <main className="bg-gray-900">
-      <Title />
-    </main>
+    <>
+      <Header />
+      <main className="bg-gray-900">
+        <Title />
+      </main>
+      <Footer />
+    </>
   );
 }
