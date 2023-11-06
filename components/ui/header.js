d8ebc91cee71807967a8f0c64f3cc35dd1c6bc90
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-11-06 15:15:33 -0300

    underline underline-offset-8 en navegacion

--- components/ui/header.js
+++ components/ui/header.js
@@ -9,14 +9,16 @@ import {
   XMarkIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const navigation = [
-  { name: "Marketplace", href: "#" },
+  { name: "Marketplace", href: "/" },
   { name: "Compania", href: "/about" },
 ];
 
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const pathname = usePathname();
 
   return (
     <header className="bg-white">
@@ -34,13 +36,15 @@ export default function Header() {
 
         <div className="hidden lg:flex lg:gap-x-12">
           {navigation.map((item) => (
-            <a
+            <Link
               key={item.name}
               href={item.href}
-              className="text-sm font-semibold leading-6 text-gray-900"
+              className={`${
+                pathname === item.href ? " underline underline-offset-8" : ""
+              } text-sm leading-6 font-semibold text-gray-900`}
             >
               {item.name}
-            </a>
+            </Link>
           ))}
         </div>
         <div className="flex flex-1 justify-end">
@@ -72,10 +76,10 @@ export default function Header() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+            <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Capellari</span>
               {/* logo de la empresa */}
-            </a>
+            </Link>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -89,13 +93,18 @@ export default function Header() {
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
                     href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    className={`${
+                      pathname === item.href
+                        ? "underline underline-offset-8"
+                        : ""
+                    } -mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7  hover:bg-gray-50'
+                `}
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
               <div className="py-6">
