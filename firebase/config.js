d52fb7ea81969f276535feaa17bca59750fe73e7
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-12-14 03:11:17 -0300

    firebase auth

--- firebase/config.js
+++ firebase/config.js
@@ -5,6 +5,7 @@ import { getFirestore } from "@firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getStorage } from "@firebase/storage";
+import { getAuth } from "@firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -22,3 +23,4 @@ const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const auth = getAuth(app);
