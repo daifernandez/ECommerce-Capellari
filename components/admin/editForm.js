807fc4d6f3b7ba22c0d86c63232e59776d20ff59
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-12-15 01:57:16 -0300

    modifico ui en EditForm

--- components/admin/editForm.js
+++ components/admin/editForm.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { db, storage } from "@/firebase/config";
 import { doc, updateDoc } from "firebase/firestore";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import CATEGORIES from "@/data/categories";
 
 const updateProduct = async (slug, values, file) => {
   let fileURL = values.image;
@@ -27,7 +28,7 @@ const updateProduct = async (slug, values, file) => {
 };
 
 export default function EditForm({ item }) {
-  const { title, description, inStock, price, category, image } = item;
+  const { title, description, inStock, price, category, image, brand } = item;
   const [values, setValues] = useState({
     title,
     description,
@@ -35,6 +36,7 @@ export default function EditForm({ item }) {
     price,
     image,
     category,
+    brand,
   });
   const [file, setFile] = useState(null);
 
@@ -52,64 +54,146 @@ export default function EditForm({ item }) {
   };
 
   return (
-    <div className="container m-auto mt-6 max-w-lg">
-      <form onSubmit={handleSubmit} className="my-12">
-        <label>Nombre: </label>
-        <input
-          type="text"
-          value={values.title}
-          required
-          className="p-2 rounded w-full border border-blue-100 block my-4"
-          name="title"
-          onChange={handleChange}
-        />
+    <div className="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+      <div className="space-y-10 divide-y divide-gray-900/10">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-8 md:grid-cols-3">
+          <div className="px-4 sm:px-0">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Editar Producto
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              Modificar los datos del producto
+            </p>
+          </div>
 
-        <label>Imagen: </label>
-        <input
-          type="file"
-          onChange={(e) => setFile(e.target.files[0])}
-          className="p-2 rounded w-full border border-blue-100 block my-4"
-        />
-
-        <label>Precio: </label>
-        <input
-          value={values.price}
-          required
-          className="p-2 rounded w-full border border-blue-100 block my-4"
-          name="price"
-          onChange={handleChange}
-        />
-
-        <label>Stock: </label>
-        <input
-          type="number"
-          value={values.inStock}
-          required
-          className="p-2 rounded w-full border border-blue-100 block my-4"
-          name="inStock"
-          onChange={handleChange}
-        />
-
-        <label>Categoria: </label>
-        <input
-          type="text"
-          value={values.category}
-          required
-          className="p-2 rounded w-full border border-blue-100 block my-4"
-          name="category"
-          onChange={handleChange}
-        />
-
-        <label>Descripción: </label>
-        <textarea
-          value={values.description}
-          className="resize-none w-full h-24 p-2 rounded border block border-blue-100 my-4"
-          name="description"
-          onChange={handleChange}
-        />
-
-        {/* <Boton type="submit">Enviar</Boton> */}
-      </form>
+          <form
+            onSubmit={handleSubmit}
+            className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2"
+          >
+            <div className="px-4 py-6 sm:p-8">
+              <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="sm:col-span-4">
+                  <label className="block text-sm font-medium leading-6 text-gray-900">
+                    Titulo
+                  </label>
+                  <div className="mt-2">
+                    <div className="mb-5 flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-slate-600 sm:max-w-md">
+                      <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+                      <input
+                        value={values.title}
+                        required
+                        type="text"
+                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                        onChange={(e) => handleChange(e)}
+                      />
+                    </div>{" "}
+                    <div className="sm:col-span-2">
+                      <label className="block text-sm font-medium leading-6 text-gray-900">
+                        Marca
+                      </label>
+                      <div className="mt-2 mb-5">
+                        <input
+                          value={values.brand}
+                          required
+                          type="text"
+                          name="brand"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
+                          onChange={handleChange}
+                        />
+                      </div>
+                    </div>
+                    <label>Imagen: </label>
+                    <input
+                      type="file"
+                      onChange={(e) => setFile(e.target.files[0])}
+                      className="p-2 rounded w-full border border-blue-100 block my-4"
+                    />
+                    <div className="sm:col-span-2 sm:col-start-1">
+                      <label className="block text-sm font-medium leading-6 text-gray-900">
+                        Precio
+                      </label>
+                      <div className="mt-2 mb-5">
+                        <input
+                          value={values.price}
+                          required
+                          type="text"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
+                          onChange={handleChange}
+                        />
+                      </div>
+                    </div>
+                    <div className="sm:col-span-2">
+                      <label className="block text-sm font-medium leading-6 text-gray-900">
+                        Stock
+                      </label>
+                      <div className="mt-2 mb-5">
+                        <input
+                          value={values.inStock}
+                          required
+                          type="number"
+                          name="inStock"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
+                          onChange={handleChange}
+                        />
+                      </div>
+                    </div>
+                    <div className="sm:col-span-4">
+                      <label className="block text-sm font-medium leading-6 text-gray-900">
+                        Categoria
+                      </label>
+                      <div className="mt-2 mb-5">
+                        <select
+                          value={values.category || ""}
+                          required
+                          id="category"
+                          name="category"
+                          autoComplete="category"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                          onChange={handleChange}
+                        >
+                          <option value="" disabled>
+                            Seleccione una categoria
+                          </option>
+                          {CATEGORIES.filter(
+                            (category) => category.id != "todos"
+                          ).map((category) => (
+                            <option key={category.id} value={category.id}>
+                              {category.label}
+                            </option>
+                          ))}
+                        </select>
+                      </div>
+                    </div>
+                    <div className="col-span-full">
+                      <label className="block text-sm font-medium leading-6 text-gray-900">
+                        Description
+                      </label>
+                      <div className="mt-2 mb-5">
+                        <textarea
+                          required
+                          rows={3}
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
+                          value={values.description}
+                          onChange={handleChange}
+                        />
+                      </div>
+                      <p className="mt-3 text-sm leading-6 text-gray-600">
+                        Modificar la descripción del producto
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="bg-slate-500 text-white px-3 py-1 rounded"
+            >
+              Guardar
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
